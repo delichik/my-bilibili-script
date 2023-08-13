de765fd93c8c03f46e2284fdbf75bd56a1739ece
delichik <delichik@imiku.moe>
2023-08-13 21:30:09 +0800

    Update emby-danmuku.js

--- emby-danmuku.js
+++ emby-danmuku.js
@@ -1,32 +1,18 @@
 // ==UserScript==
-// @name        Emby danmuku
-// @namespace   Violentmonkey Scripts
-// @grant       none
-// @version     1.0.1
-// @author      delichik
-// @downloadURL     https://raw.githubusercontent.com/delichik/my-script/main/emby-danmuku.js
-// @description 2023/8/11 23:14:11
-// @match        *://*/web/index.html
+// @name          Emby danmuku
+// @namespace     Violentmonkey Scripts
+// @grant         none
+// @version       1.0.2
+// @author        delichik
+// @downloadURL   https://raw.githubusercontent.com/delichik/my-script/main/emby-danmuku.js
+// @description   2023/8/11 23:14:11
+// @run-at        context-menu
+// @match         *://*/web/index.html
 // ==/UserScript==
 
 
 (function () {
   'use strict';
-
-  function loadJs(srcUrl) {
-    return new Promise((resolve, reject) => {
-      const script = document.createElement('script')
-      script.type = 'text/javascript'
-      script.src = srcUrl
-      document.body.appendChild(script)
-      script.onload = () => {
-        resolve()
-      }
-      script.onerror = (err) => {
-        reject(err)
-      }
-    })
-  }
 
   function loadCss(hrefUrl) {
     return new Promise((resolve, reject) => {
@@ -37,19 +23,17 @@
       link.rel = 'stylesheet'
       link.href = hrefUrl
       document.head.appendChild(link)
-      link.onload = () => {
-        resolve()
-      }
-      link.onerror = (err) => {
-        reject(err)
-      }
+      // link.onload = () => {
+      //   resolve()
+      // }
+      // link.onerror = (err) => {
+      //   reject(err)
+      // }
     })
   }
-
-  var old = window.onload
-  window.onload = function() {
-    old();
-    loadJs('https://danmaku.movie.kg/ext.js');
-    loadCss('https://danmaku.movie.kg/ext.css');
-  }
+  GM_addElement('script', {
+    src: 'https://danmaku.movie.kg/ext.js',
+    type: 'text/javascript'
+  });
+  loadCss('https://danmaku.movie.kg/ext.css');
 })();
