c7cc6de1c03fcc38056bb9baf5d2d9d665921830
delichik <delichik@imiku.moe>
2023-08-14 15:54:50 +0800

    Update emby-danmuku.js

--- emby-danmuku.js
+++ emby-danmuku.js
@@ -1,39 +1,28 @@
 // ==UserScript==
 // @name          Emby danmuku
 // @namespace     Violentmonkey Scripts
-// @grant         none
-// @version       1.0.2
+// @version       1.0.3
 // @author        delichik
 // @downloadURL   https://raw.githubusercontent.com/delichik/my-script/main/emby-danmuku.js
 // @description   2023/8/11 23:14:11
-// @run-at        context-menu
+// @run-at        document-end
 // @match         *://*/web/index.html
+// @grant         GM_xmlhttpRequest
+// @grant         GM_addStyle
 // ==/UserScript==
-
 
 (function () {
   'use strict';
-
-  function loadCss(hrefUrl) {
-    return new Promise((resolve, reject) => {
-      const linkNodes = [].slice.call(document.querySelectorAll('link')).map(item => item.href)
-      if (linkNodes.includes(hrefUrl)) return resolve()
-      const link = document.createElement('link')
-      link.type = 'text/css'
-      link.rel = 'stylesheet'
-      link.href = hrefUrl
-      document.head.appendChild(link)
-      // link.onload = () => {
-      //   resolve()
-      // }
-      // link.onerror = (err) => {
-      //   reject(err)
-      // }
-    })
-  }
-  GM_addElement('script', {
-    src: 'https://danmaku.movie.kg/ext.js',
-    type: 'text/javascript'
+  GM_xmlhttpRequest({
+    url: 'https://danmaku.movie.kg/ext.js',
+    onload: (response) => {
+      eval(response.responseText);
+    },
   });
-  loadCss('https://danmaku.movie.kg/ext.css');
+  GM_xmlhttpRequest({
+    url: 'https://danmaku.movie.kg/ext.css',
+    onload: (response) => {
+      GM_addStyle(response.responseText);
+    },
+  });
 })();
