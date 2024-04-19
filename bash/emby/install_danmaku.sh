d295ed9ba21aa1f7ad615992f4177dba075a9302
delichik <delichik@imiku.moe>
2024-04-19 21:10:45 +0800

    feat:

--- bash/emby/install_danmaku.sh
+++ bash/emby/install_danmaku.sh
@@ -6,7 +6,7 @@ content=$(cat index.html)
 # 检查index.html是否包含danmaku.movie.kg
 if grep -q "danmaku.movie.kg" index.html; then
     echo "Index.html already contains danmaku.movie.kg, skipping insertion."
-elseinstall_danmaku.sh
+else
     code='<script type="text/javascript" src="https://raw.githubusercontent.com/delichik/my-script/main/bash/emby/danmaku.movie.kg.js"></script>\n<link rel="stylesheet" href="https://raw.githubusercontent.com/delichik/my-script/main/bash/emby/danmaku.movie.kg.css"/>\n'
     new_content=$(echo -e "${content/<\/body>/$code<\/body>}")
     echo -e "$new_content" > index.html
